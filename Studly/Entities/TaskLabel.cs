294c4f9b103ca0dd5db7c1ca473cfd77e4744c13
Nazar <nazar.drahusevych@gmail.com>
2023-09-13 16:21:26 +0300

    Add entities

---
+++ Studly/Entities/TaskLabel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Studly.Entities;
+
+public class TaskLabel
+{
+    [Key]
+    public int TaskLabelId { get; set; }
+    public int TaskId { get; set; }
+    public Task? Task { get; set; }
+    public int LabelId { get; set; }
+    public Label? Label { get; set; }
+}
---
+++ Studly/Entities/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studly.Entities
+{
+    public class Timer
+    {
+        [Key]
+        public int TimerId { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime? EndTime { get; set; }
+
+        public int TaskId { get; set; }
+        public Task? Task { get; set; }
+    }
+}
