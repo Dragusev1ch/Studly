a3360483dc85d94ddcbdbd5899025f36c908eecd
Nazar <nazar.drahusevych@gmail.com>
2023-09-14 21:53:12 +0300

    Add customer service and service module

---
+++ Studly.BLL/Interfaces/ICustomerService.cs
@@ -0,0 +1,11 @@
+﻿using Studly.BLL.DTO;
+
+namespace Studly.BLL.Interfaces;
+
+public interface ICustomerService
+{
+    void CreateCustomer(CustomerDTO customerDto);
+    CustomerDTO GetCustomerById(int? id);
+    IEnumerable<CustomerDTO> GetCustomers();
+    void Dispose();
+}
