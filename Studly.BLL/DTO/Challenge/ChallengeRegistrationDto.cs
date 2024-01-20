6f13bdd1d040be486cba07d46ac14bdda3abc86f
Yevhenii Hryn <6867668@stud.nau.edu.ua>
2024-01-20 18:01:17 +0200

    Edited the ability to create Challenges:
    ✓ Removed the ability to create subtasks immediately when creating a task.
    
    ✓ Added validation of the task creation model (added fences to the fields).

--- Studly.BLL/DTO/Challenge/ChallengeRegistrationDto.cs
+++ Studly.BLL/DTO/Challenge/ChallengeRegistrationDto.cs
@@ -1,13 +1,20 @@
-﻿using Studly.DAL.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using Studly.DAL.Enums;
 
 namespace Studly.BLL.DTO.Challenge;
 
 public class ChallengeRegistrationDto
 {
-    public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    [Required]
+    [StringLength(50, ErrorMessage = "Title limit is 50 chars!")]
+    public string Title { get; set; }
+
+    [StringLength(1000, ErrorMessage = "Maximum length is 1000 chars!")]
+    public string Description { get; set; }
+
     public DateTime? Deadline { get; set; }
-    public ChallengePriority Priority { get; set; }
-    public ChallengeStatus Status { get; set; }
-    public List<ChallengeRegistrationDto> SubTasks { get; set; } = new();
+
+    [Required] public ChallengePriority Priority { get; set; }
+
+    public ChallengeStatus Status { get; set; } = ChallengeStatus.NotStarted;
 }
